18416765c23cab3bee99790e2ede61f4e0b57226
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2022-09-02 03:51:16 +0200

    Logging

--- src/main/java/com/quimify/servidor/masa_molecular/MasaMolecularController.java
+++ src/main/java/com/quimify/servidor/masa_molecular/MasaMolecularController.java
@@ -1,11 +1,7 @@
 package com.quimify.servidor.masa_molecular;
 
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 
 // Esta clase implementa los métodos HTTP de la dirección "/masamolecular".
 
