c9455542806e0b1e015189a019480756ceee6357
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2022-10-23 19:29:45 +0200

    Organic is now in another repository

--- src/main/java/com/quimify/api/organic/OrganicController.java
+++ src/main/java/com/quimify/api/organic/OrganicController.java
@@ -1,5 +1,6 @@
 package com.quimify.api.organic;
 
+import com.quimify.organic.OrganicResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
