b2ffe01039edb0b5a0129c8e3f39ed29a9dfc72e
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2023-05-05 17:17:23 +0200

    Try to fix CPU bug using ReadWriteLock

--- src/main/java/com/quimify/api/inorganic/AutocompleteComponent.java
+++ src/main/java/com/quimify/api/inorganic/AutocompleteComponent.java
@@ -10,6 +10,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 // This class solves inorganic completions.
 
@@ -26,34 +28,29 @@ class AutocompleteComponent { // TODO rename "CompleteComponent"
     @Autowired
     ErrorService errorService; // API errors logic
 
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock(); // TODO comment about it
     private final Map<String, Integer> normalizedTextToId = new LinkedHashMap<>(); // TODO concurrent? synchronized?
 
     // Administration:
 
     @Scheduled(fixedDelay = 5 * 1000) // At startup, then once every 5 seconds // TODO fix time
     private void tryUpdateCache() {
+        readWriteLock.writeLock().lock();
+
         try {
             updateCache();
         } catch (Exception exception) {
             errorService.log("Exception updating cache", exception.toString(), this.getClass());
+        } finally {
+            readWriteLock.writeLock().unlock();
         }
     }
 
-    private boolean firstTime = true;
-    List<InorganicModel> inorganicModels;
-
     private void updateCache() {
-        if (firstTime) {
-            this.inorganicModels = inorganicRepository.findAllByOrderBySearchCountDesc();
-            firstTime = false;
-        }
-
-        List<InorganicModel> inorganicModels = new ArrayList<>(this.inorganicModels);
+        List<InorganicModel> inorganicModels = inorganicRepository.findAllByOrderBySearchCountDesc();
 
         for (InorganicModel inorganicModel : inorganicModels)
             putNormalized(inorganicModel);
-
-        firstTime = false;
 
         logger.info("Inorganic completion cache updated.");
     }
@@ -84,18 +81,19 @@ class AutocompleteComponent { // TODO rename "CompleteComponent"
     // Internal:
 
     protected String tryAutoComplete(String input) { // TODO rename "tryComplete"
+        readWriteLock.readLock().lock();
+
         try {
             return autoComplete(input);
         } catch (Exception exception) {
             errorService.log("Exception completing: " + input, exception.toString(), this.getClass());
             return "";
+        } finally {
+            readWriteLock.readLock().unlock();
         }
     }
 
     private String autoComplete(String input) { // TODO rename "complete"
-        if (firstTime)
-            return "";
-
         String normalizedInput = Normalizer.get(input);
 
         for (Map.Entry<String, Integer> entry : normalizedTextToId.entrySet())
