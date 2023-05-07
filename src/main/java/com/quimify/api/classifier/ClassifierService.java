f9fc638d32bd063d2267d210b7f153416c7a169a
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2023-05-07 02:25:42 +0200

    Add connections to Quimify Classifier API

---
+++ src/main/java/com/quimify/api/classifier/ClassifierService.java
@@ -0,0 +1,44 @@
+package com.quimify.api.classifier;
+
+import com.quimify.api.error.ErrorService;
+import com.quimify.api.settings.SettingsService;
+import com.quimify.api.utils.Connection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+// This class handles calls to the Quimify Classifier API.
+
+@Service
+public
+class ClassifierService {
+
+    @Autowired
+    SettingsService settingsService; // Settings logic
+
+    // TODO metrics
+
+    @Autowired
+    ErrorService errorService; // API errors logic
+
+    static final int notFoundResultCode = -1;
+
+    // Public:
+
+    public Optional<ClassifierResult> classify(String text) {
+        try {
+            String response = new Connection(settingsService.getClassifierUrl(), text).getText();
+            int result = Integer.parseInt(response);
+
+            if (result == notFoundResultCode)
+                return Optional.empty();
+
+            return Optional.of(ClassifierResult.values()[result]);
+        } catch (Exception exception) {
+            errorService.log("Exception calling classifier for: " + text, exception.toString(), getClass());
+            return Optional.empty();
+        }
+    }
+
+}
