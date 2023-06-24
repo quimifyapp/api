277482010972ee83c741225861f29b1fcc653002
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2023-06-24 18:17:26 +0200

    Add ClassifierService.isChemistryProblem

--- src/main/java/com/quimify/api/classifier/ClassifierService.java
+++ src/main/java/com/quimify/api/classifier/ClassifierService.java
@@ -6,6 +6,7 @@ import com.quimify.api.utils.Connection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 // This class handles calls to the Quimify Classifier API.
@@ -23,15 +24,34 @@ public class ClassifierService {
 
     // Constants:
 
-    static final int notFoundResultCode = -1;
+    private static final int notFoundResultCode = -1;
 
-    // TODO problems vocabulary
+    private static final List<String> chemistryProblemWords = List.of( // TODO test with all DB and random organics
+            "el", "en", "ml",
+            "con", "del", "las", "los ", "por", "que", "una",
+            "cada", "cual", "masa", "para", "moles",
+            "tiene", "gramos",
+            "litros", "numero",
+            "presion", "volumen",
+            "cantidad", "contiene", "preparar", "reaccion", "solucion",
+            "disolucion", "porcentaje", "siguientes",
+            "temperatura",
+            "concentracion"
+    );
+
+    // TODO reactions vocabulary
 
     // Public:
 
-    public Optional<ClassifierResult> classify(String text) {
+    public Optional<ClassifierResult> classify(String input) {
         try {
-            String response = new Connection(settingsService.getClassifierUrl(), text).getText();
+            if(isChemicalReaction(input))
+                return Optional.of(ClassifierResult.chemicalReaction);
+
+            if(isChemistryProblem(input))
+                return Optional.of(ClassifierResult.chemistryProblem);
+
+            String response = new Connection(settingsService.getClassifierUrl(), input).getText();
             int result = Integer.parseInt(response);
 
             if (result == notFoundResultCode)
@@ -39,13 +59,24 @@ public class ClassifierService {
 
             return Optional.of(ClassifierResult.values()[result]);
         } catch (Exception exception) {
-            errorService.log("Exception calling classifier for: " + text, exception.toString(), getClass());
+            errorService.log("Exception calling classifier for: " + input, exception.toString(), getClass());
             return Optional.empty();
         }
     }
 
     public boolean isOrganic(ClassifierResult result) {
         return result == ClassifierResult.organicFormula || result == ClassifierResult.organicName;
+    }
+
+    // Private:
+
+    private boolean isChemicalReaction(String input) {
+        // TODO
+        return false;
+    }
+
+    private boolean isChemistryProblem(String input) {
+        return chemistryProblemWords.stream().anyMatch((input::contains));
     }
 
 }
