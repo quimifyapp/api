5a4d390a0785dbd964f2d3cf131ad34d254d2898
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2024-05-02 16:45:55 +0200

    Update version 5 -> 6

--- sql/settings/INSERT settings.sql
+++ sql/settings/INSERT settings.sql
@@ -5,7 +5,7 @@ VALUES(
 true, 33, # Free Bing
 true, 100, # Google
 "https://api.bing.microsoft.com/v7.0/search?count=1&q=site:www.formulacionquimica.com+", # Free Bing URL
-"https://www.googleapis.com/customsearch/v1/siterestrict?key=%s&cx=f475470fccb0e037e&num=1&q=", # Google URL
-"http://localhost:8000", # Classifier URL
+"https://www.googleapis.com/customsearch/v1/siterestrict?key=%s&cx=773fc1cf8a92a4979&num=1&q=", # Google URL
+"http://localhost:8070/?input=", # Classifier URL
 "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.147 Safari/537.36" # User agent
 );
