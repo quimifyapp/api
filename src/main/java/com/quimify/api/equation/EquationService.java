50815eac22964d68ebb137b27243b7c646d05891
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2024-11-19 19:10:35 +0100

    Remove TODO

--- src/main/java/com/quimify/api/equation/EquationService.java
+++ src/main/java/com/quimify/api/equation/EquationService.java
@@ -12,8 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-// TODO rename Equation to Reaction
-
 @Service
 class EquationService {
 
